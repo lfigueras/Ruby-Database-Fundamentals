350088d361ad3f3e1b8613c92b0b1085e387e713
Lovely Joy Figueras <54517697+lfigueras@users.noreply.github.com>
2020-11-07 11:06:19 +0800

    Updated database.sql

--- database.sql
+++ database.sql
@@ -23,3 +23,74 @@ SET first_name = 'Ana', middle_name = 'Cui', last_name = 'Cajocson', age = 25, l
 WHERE id = 1
 
 DELETE FROM students WHERE id = 6;
+
+                                                                                                                                         
+                                                                                                                                         
+SELECT COUNT (id)
+FROM students
+
+SELECT *
+FROM students
+WHERE location = 'Manila'
+
+SELECT *
+FROM students
+ORDER BY age DESC;
+
+=================
+
+
+
+
+
+
+
+CREATE TABLE classrooms (id integer, student_id integer, section varchar(2));
+
+INSERT INTO classrooms (id, student_id, section)
+VALUES(1, 1, 'A');
+INSERT INTO classrooms (id, student_id, section)
+VALUES(2, 2, 'A');
+INSERT INTO classrooms (id, student_id, section)
+VALUES(3, 3, 'B');
+INSERT INTO classrooms (id, student_id, section)
+VALUES(4, 4, 'C');
+INSERT INTO classrooms (id, student_id, section)
+VALUES(5, 5, 'B');
+INSERT INTO classrooms (id, student_id, section)
+VALUES(6, 6, 'A');
+INSERT INTO classrooms (id, student_id, section)
+VALUES(7, 7, 'C');
+INSERT INTO classrooms (id, student_id, section)
+VALUES(8, 8, 'B');
+INSERT INTO classrooms (id, student_id, section)
+VALUES(9, 9, 'B');
+INSERT INTO classrooms (id, student_id, section)
+VALUES(10, 10, 'C');
+
+
+##innerjoin ##leftjoin ##rightjoin ##fulljoin
+
+select first_name, last_name, id, student_id, section
+from students s
+inner join classrooms c
+on s.id = c.student_id
+
+
+select first_name, last_name,  student_id, section
+from students s
+left join classrooms c
+on s.id = c.student_id
+
+
+select first_name, last_name,  student_id, section
+from students s
+right join classrooms c
+on s.id = c.student_id
+
+select first_name, last_name,  student_id, section
+from students s
+full join classrooms c
+on s.id = c.student_id
+
+
